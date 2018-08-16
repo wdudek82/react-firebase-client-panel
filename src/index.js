a4b900dd46a65d9bf37cf33a4d6717673c4ae0df
Wojtek Dudek <wdudek82@gmail.com>
2018-08-16 13:02:03 +0200

    Added eslint config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+library.add(faStroopwafel);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
